7fcaad4b451ba80e0882aa897b1b15b4a5641139
yangying <yangying@blackcrystalinfo.com>
2014-09-23 17:29:23 +0800

    add sentinels lib

---
+++ sentinels/sentinels.go
@@ -0,0 +1,191 @@
+package sentinels
+
+import (
+	"errors"
+	"time"
+
+	"github.com/fzzy/radix/extra/sentinel"
+	"github.com/fzzy/radix/redis"
+)
+
+var (
+	ErrNoSentinel = errors.New("all sentinels were not avaliable")
+	ErrEmptyName = errors.New("names of redis cannot be empty")
+)
+
+type putChan struct {
+	name	string
+	client	*redis.Client
+}
+
+type Sentinels struct {
+	addrs		[]string
+	curIndex	int
+	curClient	*sentinel.Client
+	names		[]string
+	poolSize	int
+
+	errCh		chan struct{}
+	getCh		chan chan *sentinel.Client
+	putCh		chan putChan
+	downCh		chan struct{}
+	newCh		chan *sentinel.Client
+
+	quitCh		chan struct{}
+}
+
+func NewSentinels(addrs []string, poolSize int, names ...string) (*Sentinels, error) {
+	if len(addrs) == 0 {
+		return nil, ErrEmptyName
+	}
+	s := &Sentinels{
+		addrs:		addrs,
+		curIndex:	-1,
+		names:		names,
+		errCh:		make(chan struct{}),
+		getCh:		make(chan chan *sentinel.Client, 128),
+		putCh:		make(chan putChan, 128),
+		downCh:		make(chan struct{}),
+		newCh:		make(chan *sentinel.Client),
+		quitCh:		make(chan struct{}),
+	}
+	go s.maker()
+	go s.handler()
+
+	s.dial(false)
+
+	client := s.getSentinel(0)
+	if client == nil {
+		return nil, ErrNoSentinel
+	}
+
+	return s, nil
+}
+
+func (s *Sentinels) dial(aroundLoop bool) {
+	for {
+		s.curIndex++
+		if s.curIndex >= len(s.addrs) {
+			s.curIndex = 0
+		}
+		client, err := sentinel.NewClient("tcp", s.addrs[s.curIndex], s.poolSize, s.names...)
+		if err == nil {
+			select {
+			case s.newCh <- client:
+			default:
+				client.Close()
+			}
+			break
+		}
+		if (len(s.addrs) - 1) == s.curIndex && !aroundLoop {
+			break
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+func (s *Sentinels) Close() {
+	close(s.quitCh)
+}
+
+func (s *Sentinels) handler() {
+	for {
+		select {
+		case <-s.quitCh:
+			close(s.errCh)
+			close(s.getCh)
+			close(s.putCh)
+			close(s.newCh)
+			return
+
+		case <-s.errCh:
+			s.curClient.Close()
+			s.curClient = nil
+
+			select {
+			case s.downCh <- struct{}{}:
+			default:
+			}
+
+		case getter := <-s.getCh:
+			select {
+			case getter <- s.curClient:
+			default:
+				break
+			}
+
+		case putter := <-s.putCh:
+			if s.curClient == nil {
+				putter.client.Close()
+			} else {
+				s.curClient.PutMaster(putter.name, putter.client)
+			}
+
+		case ns := <-s.newCh:
+			s.curClient = ns
+		}
+	}
+}
+
+func (s *Sentinels) maker() {
+	for {
+		select {
+		case <-s.quitCh:
+			close(s.downCh)
+			return
+
+		case <-s.downCh:
+			s.dial(true)
+		}
+	}
+}
+
+func (s *Sentinels) GetMaster(name string) (*redis.Client, error) {
+	return s.GetMasterTimeout(name, 0)
+}
+
+func (s *Sentinels) getSentinel(timeout time.Duration) (*sentinel.Client) {
+	ch := make(chan *sentinel.Client)
+	var client *sentinel.Client
+	if timeout > 0 {
+		select {
+		case s.getCh <- ch:
+		case <-time.After(timeout):
+		}
+		select {
+		case client = <-ch:
+		case <-time.After(timeout):
+		}
+	} else {
+		s.getCh <- ch
+		client = <-ch
+	}
+	close(ch)
+
+	return client
+}
+
+func (s *Sentinels) GetMasterTimeout(name string, timeout time.Duration) (*redis.Client, error) {
+	client := s.getSentinel(timeout)
+	if client == nil {
+		return nil, ErrNoSentinel
+	}
+	return client.GetMaster(name)
+}
+
+func (s *Sentinels) PutMaster(name string, client *redis.Client) {
+	s.PutMasterTimeout(name, client, 0)
+}
+
+func (s *Sentinels) PutMasterTimeout(name string, client *redis.Client, timeout time.Duration) {
+	putter := putChan{name, client}
+	if timeout > 0 {
+		select {
+		case s.putCh <- putter:
+		case <-time.After(timeout):
+		}
+	} else {
+		s.putCh <- putter
+	}
+}
+
